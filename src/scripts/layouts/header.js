4a7c415c9f2d92de535559dc795fd03d66f5f8c7
상록에스-웹사업\Sangrok_Dev <xowns4851491@gmail.com>
2025-09-01 17:11:43 +0900

    no message

--- src/scripts/layouts/header.js
+++ src/scripts/layouts/header.js
@@ -2,55 +2,19 @@
 $(() => {
     let moveArrow = '';
     let oldTop = 0;
-          
-
-    $("#wrap").append('<div class="blind d-none"></div>');    
+      
 
     // 메인 네비
     $(".gnb-menu li").each(function () {
         const btn = $(this).find("button");
         btn.on('click', function (){
             $(".gnb-menu li").removeClass('active');
-            $(this).parent().addClass('active');
-            $('body').css({'overflow': 'hidden'});
+            $(this).parent().addClass('active');            
             $(".main-allmenu").hide();
             $("#header .allmenu").removeClass('active');
             $(".mobile-dep-menu").removeClass('mobile-active');            
         });
-    });
-
- 
-
-    // 스크롤 이벤트
-    $("html, body").on("scroll", (e) => {
-        const top = $('body').scrollTop();
-        if(oldTop < top) {
-            moveArrow = 'down';
-        } else {
-            moveArrow = 'up';
-        }
-        if(moveArrow === 'down' && top > 200) {
-            if(!$("#header").hasClass('hide')) {
-                $("#header").addClass('hide');
-            }
-        } else if(moveArrow === 'up') {
-            if($("#header").hasClass('hide')) {
-                $("#header").removeClass('hide');
-            }
-        }
-
-        if(top >= 32) {
-            if(!$(".main-allmenu").hasClass("large")) {
-                $(".main-allmenu").addClass("large");
-            }
-        } else {
-            if($(".main-allmenu").hasClass("large")) {
-                $(".main-allmenu").removeClass("large");
-            }
-        }
-    
-        oldTop = top;
-    });   
+    });  
 });
 
 
