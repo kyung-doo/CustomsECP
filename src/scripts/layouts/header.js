e0729059112293a688931d89b95cfb33258754d1
상록에스-웹사업\Sangrok_Dev <xowns4851491@gmail.com>
2026-01-13 16:26:17 +0900

    no message

--- src/scripts/layouts/header.js
+++ src/scripts/layouts/header.js
@@ -34,6 +34,28 @@ $(() => {
             th.closest('.popup-box').find('.btn-navi.popup').removeClass('active');            
         })
     });
+
+
+    //메뉴 js
+    $(function () {
+    $('.depth2 > li > a').on('click', function (e) {
+        e.preventDefault();
+
+        var $depth3 = $(this).next('.depth3');
+
+        // depth3가 있을 때만 동작
+        if ($depth3.length) {
+            // 다른 열린 메뉴 닫기
+            $('.depth3').not($depth3).slideUp(300);
+            $('.depth2 > li > a').not(this).removeClass('on');
+
+            // 현재 메뉴 토글
+            $depth3.slideToggle(300);
+            $(this).toggleClass('on');
+        }
+    });
+});
+
 });
 
 
